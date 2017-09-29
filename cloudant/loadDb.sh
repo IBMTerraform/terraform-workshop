59c7796277b031577b2f6e54e7a3c924b55ff291
Matthias Kubik <kubik@de.ibm.com>
2017-09-29 11:56:27 +0200

    modification for date import and commenting

---
+++ cloudant/loadDb.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+DBURL=$1
+echo $DBURL
+
+curl -X PUT -H "Accept: application/json" -H 'Content-Type:application/json' $DBURL/products
+echo RC=$?
+curl -X PUT -H "Accept: application/json" -H 'Content-Type:application/json' $DBURL/categories
+echo RC=$?
+echo curl -d @./categories.json -X POST -H 'Accept: application/json' -H 'Content-Type:application/json' $DBURL/categories/_bulk_docs
+curl -d @./categories.json -X POST -H 'Accept: application/json' -H 'Content-Type:application/json' $DBURL/categories/_bulk_docs
+echo RC=$?
+echo curl -d @./products.json -X POST -H 'Accept: application/json' -H 'Content-Type:application/json' $DBURL/products/_bulk_docs
+curl -d @./products.json -X POST -H 'Accept: application/json' -H 'Content-Type:application/json' $DBURL/products/_bulk_docs
+echo RC=$?
--- cloudant/provider.tf
+++ cloudant/provider.tf
@@ -1,4 +1,6 @@
+################################################
 # Configure the IBM Cloud Provider
+################################################
 provider "ibm" {
   bluemix_api_key = "${var.ibm_bmx_api_key}"
 }
